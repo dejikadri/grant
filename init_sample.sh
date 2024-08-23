7ad44692f1adb430404cf7c1f1cc1a9e159ef4d2
Bash Kadri <dejikadri@google.com>
2024-08-23 00:22:14 -0400

    created djangoand drf and dockercompose

---
+++ init_sample.sh
@@ -0,0 +1,9 @@
+export MYSQL_ROOT_PASSWORD=root-password
+export MYSQL_DATABASE=the-databse-name
+export MYSQL_USER=the-user
+export MYSQL_PASSWORD=the-user-password
+
+echo $MYSQL_ROOT_PASSWORD
+echo $MYSQL_DATABASE
+echo $MYSQL_USER
+echo $MYSQL_PASSWORD
