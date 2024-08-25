4630fe5def401caad70e2c6241dae4498ef66db0
Deji Kadri <dejikadri@gmail.com>
2024-08-24 22:32:57 -0400

    Add validator to grant apply model

--- grants/models.py
+++ grants/models.py
@@ -1,9 +1,16 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 import uuid
+from django.core.validators import FileExtensionValidator
 
 # function to get 1st 12 characters of the uuid
 def get_uuid():
     return str(uuid.uuid4())[:12]
+
+def validate_file_size(file):
+    limit = 10 * 1024 * 1024  # 10 MB limit
+    if file.size > limit:
+        raise ValidationError(_('File size should not exceed 10 MB.'))
 
 # a model for the grant application
 class Grant(models.Model):
@@ -22,9 +29,9 @@ class Grant(models.Model):
 class GrantApplication(models.Model):
     grant = models.ForeignKey(Grant, on_delete=models.CASCADE)
     user = models.ForeignKey('users.User', on_delete=models.CASCADE)
-    document = models.FileField(upload_to='documents/')
-    document_name = models.CharField(max_length=255)
-    application_text = models.TextField()
+    document = models.FileField(upload_to='documents/', validators=[validate_file_size])
+    document_name = models.CharField(max_length=255, null=False, blank=False)
+    application_text = models.TextField(blank=False, null=False)
     submission_status = models.CharField(max_length=20, default='draft')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
